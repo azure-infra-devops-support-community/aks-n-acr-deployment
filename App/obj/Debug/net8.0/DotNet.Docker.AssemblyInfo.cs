1617344bfd8928f42c58b6eaa5b9c60f55c3b32c
nacho <49342621+iheanachochukwu@users.noreply.github.com>
2024-10-31 17:09:40 +0000

    hello dotnet app and dockerfile

--- App/obj/Debug/net8.0/DotNet.Docker.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/DotNet.Docker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNet.Docker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8e0b29525311f7556efca892259242f884b965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc37761052aa10f0c493e90b006870987e1cfdcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNet.Docker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNet.Docker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
